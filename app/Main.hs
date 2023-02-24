6a06afc985fd0ef67c757cc453bcdb41cd1f246b
Henrique Gasques <henriquegasques@gmail.com>
2023-02-23 23:53:43 -0300

    Install Brick and use it to display a simple message

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,8 @@
 module Main (main) where
 
-import Lib
+import Brick
+
+import Ui
 
 main :: IO ()
-main = someFunc
+main = simpleMain message
---
+++ src/Ui.hs
@@ -0,0 +1,8 @@
+module Ui
+    ( message
+    ) where
+
+import Brick
+
+message :: Widget ()
+message = str "Checklist TUI"
